f85e7330a6070e5a43e97c806cc675c488ed9563
Полинкевич Иван Дмитриевич ИП-511 <iveng303@gmail.com>
2025-10-21 04:21:24 +0700

    1

---
+++ Car.java
@@ -0,0 +1,18 @@
+class Car{
+    private String mark;
+    private int id;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private int number;
+    public Car(String mark,int id,String model,int year,String color,int price,int number){
+        this.mark =mark;
+        this.id = id;
+        this model = model;
+        this year = year;
+        this color = color;
+        this price = price;
+        this number = number;
+    }
+}
